71dc7249ee32270aa5f63a33a88d106598db7deb
Koleman Nix <kolemannix@gmail.com>
2014-03-08 17:40:14 -0500

    restructure directories
    Signed-off-by: Koleman Nix <kolemannix@gmail.com>

--- test/markov/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns markov.core-test
-  (:require [clojure.test :refer :all]
-            [markov.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
---
+++ test/unit/knix/markov.clj
@@ -0,0 +1,3 @@
+(ns unit.knix.markov
+  (:require [midje.sweet :refer :all]
+            [markov :refer :all]))
